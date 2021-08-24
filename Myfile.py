5d3ac52ba29c59d3892425c6e66f2f8591fde8a6
Solomon Taravaru <2009202101@ceitupng.com>
2021-08-25 09:16:27 +1000

    add Myfile.py

---
+++ Myfile.py
@@ -0,0 +1,10 @@
+# This program says hello and asks for my name.
+print("hello group4!")
+print("What is your name?) #ask for their name
+myName=("Solomon")
+print("It is good to meet you, "+myName)
+print("The length of your name is:")
+print("len(myname))
+print("what is your age?") #ask for their age
+myAge=input(24)
+print("you will be "+str(int(myAGE)+1)+" in a year.")
